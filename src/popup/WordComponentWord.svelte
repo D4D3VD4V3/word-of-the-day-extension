6560b256b66a69b7fd01c5307ef0c61c82ac41b6
David <D4D3VD4V3@users.noreply.github.com>
2022-06-05 13:50:18 -0400

    added attribution

--- src/popup/WordComponentWord.svelte
+++ src/popup/WordComponentWord.svelte
@@ -8,7 +8,9 @@
 <p
   class="focus:outline-none text-sm md:text-lg lg:text-xl leading-7 text-gray-700 dark:text-gray-300"
 >
-  <span class="font-bold font-serif text-md md:text-xl lg:text-2xl">{word} </span><span
-    class="font-medium font-serif text-md md:text-xl lg:text-2xl">({partOfSpeech})</span
+  <span class="font-bold font-serif text-md md:text-xl lg:text-2xl"
+    ><a href="https://www.wordnik.com/words/{word}">{word}</a>
+  </span><span class="font-medium font-serif text-md md:text-xl lg:text-2xl"
+    >({partOfSpeech})</span
   >
 </p>
